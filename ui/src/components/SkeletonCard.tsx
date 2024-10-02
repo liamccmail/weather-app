4ce14e55e3a53e6160d0661aeadd311d482bb6eb
Liam Cunliffe <lcun053@gmail.com>
2024-10-02 18:48:48 +0100

    Updates to SkeletonCard using TailwindCSS to make sizes more configurable and customizable.

--- ui/src/components/SkeletonCard.tsx
+++ ui/src/components/SkeletonCard.tsx
@@ -1,15 +1,28 @@
-import Skeleton from '@mui/material/Skeleton';
-
 export default function LoadingSkeletonCard() {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center justify-center max-w-md mx-auto">
-      <Skeleton variant="text" sx={{ fontSize: '1rem', width: 150 }} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem', width: 200 }} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem', width: 100 }} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem', width: 100 }} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem', width: 100 }} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem', width: 100 }} />
-      <Skeleton variant="text" sx={{ fontSize: '1rem', width: 100 }} />
+    <div className="bg-ghost-white p-6 rounded-lg shadow-lg flex justify-center">
+      <div className="flex-1 text-center">
+        <div className="h-6 bg-gray-300 rounded w-1/4 mx-auto mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-2/4 mx-auto mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-1/4 mx-auto"></div>
+      </div>
+
+      <div className="border-l border-gray-600 mx-4"></div>
+
+      <div className="flex-1 text-center">
+        <div className="h-6 bg-gray-300 rounded w-1/2 mx-auto mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-1/4 mx-auto mb-1"></div>
+        <div className="h-4 bg-gray-300 rounded w-3/4 mx-auto mb-1"></div>
+        <div className="h-4 bg-gray-300 rounded w-2/4 mx-auto"></div>
+      </div>
+
+      <div className="border-l border-gray-600 mx-4"></div>
+
+      <div className="flex-1 text-center">
+        <div className="h-6 bg-gray-300 rounded w-1/2 mx-auto mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded w-1/4 mx-auto mb-1"></div>
+        <div className="h-4 bg-gray-300 rounded w-1/4 mx-auto"></div>
+      </div>
     </div>
   );
 }
