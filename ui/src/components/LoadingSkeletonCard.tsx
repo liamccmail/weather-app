59f66e593eeec6bc2fffae4ac71a949094572d43
Liam Cunliffe <lcun053@gmail.com>
2024-10-03 15:21:42 +0100

    Rename SkeletonCard file to LoadingSkeletonCard

--- ui/src/components/LoadingSkeletonCard.tsx
+++ ui/src/components/LoadingSkeletonCard.tsx
@@ -1,6 +1,6 @@
 export default function LoadingSkeletonCard() {
   return (
-    <div className="bg-ghost-white p-6 rounded-lg shadow-lg flex justify-center">
+    <div data-testid="loading-skeleton" className="bg-ghost-white p-6 rounded-lg shadow-lg flex justify-center">
       <div className="flex-1 text-center">
         <div className="h-6 bg-gray-300 rounded w-1/4 mx-auto mb-2"></div>
         <div className="h-4 bg-gray-300 rounded w-2/4 mx-auto mb-2"></div>
