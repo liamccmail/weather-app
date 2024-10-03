8bd7a876241b9088f0c7be2f78ea091957474942
Liam Cunliffe <lcun053@gmail.com>
2024-10-03 17:02:05 +0100

    SwaggerUI - Updating base files to handle XML comments for API documentation.

--- api/src/WeatherService.Api/Program.cs
+++ api/src/WeatherService.Api/Program.cs
@@ -1,10 +1,23 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddUserSecrets<Program>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+  options.SwaggerDoc("v1", new OpenApiInfo
+    {
+      Version = "v1",
+      Title = "WeatherService API",
+      Description = "An ASP.NET Core Web API for fetching current and astronomy weather data from City locations.",
+    });
+
+  var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 // Add services to the container.
 
 var app = builder.Build();
@@ -12,8 +25,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI(options =>
+  {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+  });
 }
 
 // Enable CORS from any origin
