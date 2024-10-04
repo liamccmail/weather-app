ee2f85fc396971e9031ab9715e31962fc1c73218
Liam Cunliffe <lcun053@gmail.com>
2024-10-04 12:12:14 +0100

    SwaggerUI - Adding xml comments to endpoint. New packages for Swagger Docs implementation.

--- api/src/WeatherService.Api/Controllers/WeatherController.cs
+++ api/src/WeatherService.Api/Controllers/WeatherController.cs
@@ -1,20 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using WeatherService.Api.Models;
 using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace WeatherService.Api.Controllers
 {
+
+  [ApiController]
   [Route("[controller]")]
   public class WeatherController(IConfiguration configuration) : Controller
   {
     private readonly IConfiguration _configuration = configuration;
 
+    /// <summary>
+    /// Fetches current and astronomical Weather Data from a City Location.
+    /// </summary>
+    /// <param name="city">The city for which to get the weather data.</param>
+    /// <returns>Current and astronomical weather data for the searched City</returns>
+    /// <response code="200">Returns the set of current and astronomical weather data for City,</response>
+    /// <response code="404">If the city passed is not found</response>
+    /// <example>
+    /// {
+    ///     "city": "London",
+    ///     "region": "City of London, Greater London",
+    ///     "country": "United Kingdom",
+    ///     "localTime": "2021-09-30 12:00",
+    ///     "temperature": 15.0,
+    ///     "sunrise": "06:00",
+    ///     "sunset": "18:00"
+    /// }
+    /// </example>
     [HttpGet("{city}")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(Location), StatusCodes.Status200OK)]
+    [SwaggerResponseExample(200, typeof(LocationExample))]
     public async Task<IActionResult> GetLocationData(string city)
     {
       using var client = new HttpClient();
       try
       {
+        // Fetch API Key from configuration.
         var apiKey = _configuration["WeatherApi:ApiKey"];
         client.BaseAddress = new Uri("http://api.weatherapi.com");
 
@@ -54,6 +79,20 @@ namespace WeatherService.Api.Controllers
       {
         return BadRequest($"Error getting weather from WeatherApi: {httpRequestException.Message}");
       }
+    }
+
+    public class LocationExample
+    {
+        public static readonly Location Example = new()
+        {
+            City = "London",
+            Region = "City of London, Greater London",
+            Country = "United Kingdom",
+            LocalTime = "2021-09-30 12:00",
+            Temperature = 15.0F,
+            Sunrise = "06:00",
+            Sunset = "18:00"
+        };
     }
   }
 }
--- api/src/WeatherService.Api/Program.cs
+++ api/src/WeatherService.Api/Program.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +7,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => {
-  options.SwaggerDoc("v1", new OpenApiInfo
-    {
-      Version = "v1",
-      Title = "WeatherService API",
-      Description = "An ASP.NET Core Web API for fetching current and astronomy weather data from City locations.",
-    });
-
   var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
   options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
